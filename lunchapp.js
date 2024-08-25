e7316b56ed4123ad487762fe432949bb5bd22fff
aliola123 <alipoemoses6461@gmail.com>
2024-08-25 12:16:53 +0100

    Pushed the styles, images and scripts

--- lunchapp.js
+++ lunchapp.js
@@ -1,3 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", "views");
